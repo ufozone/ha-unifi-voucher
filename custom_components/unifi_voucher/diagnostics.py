e143194ba19f3ed6255e452ee38021c7af3ab256
Markus <Markus@Markus-HP840G10.localdomain>
2023-11-12 00:30:52 +0100

    Add diagnostic for config entry

---
+++ custom_components/unifi_voucher/diagnostics.py
@@ -0,0 +1,31 @@
+"""Diagnostics support for UniFi Hotspot Manager."""
+from __future__ import annotations
+
+from homeassistant.components.diagnostics import async_redact_data
+from homeassistant.const import (
+    CONF_PASSWORD,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import (
+    DOMAIN,
+)
+
+TO_REDACT = {
+    CONF_PASSWORD,
+}
+
+
+async def async_get_config_entry_diagnostics(
+    hass: HomeAssistant, config_entry: ConfigEntry
+) -> dict[str, any]:
+    """Return diagnostics of the config entry and coordinator data."""
+    coordinator = hass.data[DOMAIN][config_entry.entry_id]
+    diagnostics_data = {
+        "config_entry_data": async_redact_data(config_entry.as_dict(), TO_REDACT),
+        "coordinator_vouchers": coordinator.vouchers,
+        "coordinator_latest_voucher_id": coordinator.latest_voucher_id,
+    }
+
+    return diagnostics_data
