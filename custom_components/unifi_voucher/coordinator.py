09c13a4b60b1d584619b6b948c6eac5ca512e732
ufozone <dev@ufozone.de>
2024-04-04 01:40:33 +0200

    Fix using rate_max_up and rate_max_down by creating new voucher #41

--- custom_components/unifi_voucher/coordinator.py
+++ custom_components/unifi_voucher/coordinator.py
@@ -33,6 +33,8 @@ from .const import (
     CONF_VOUCHER_QUOTA,
     CONF_VOUCHER_DURATION,
     CONF_VOUCHER_USAGE_QUOTA,
+    CONF_VOUCHER_RATE_MAX_UP,
+    CONF_VOUCHER_RATE_MAX_DOWN,
     DEFAULT_VOUCHER,
 )
 from .api import (
@@ -249,6 +251,12 @@ class UnifiVoucherCoordinator(DataUpdateCoordinator):
 
             if usage_quota is None:
                 usage_quota = int(self.get_entry_option(CONF_VOUCHER_USAGE_QUOTA))
+
+            if rate_max_up is None:
+                rate_max_up = int(self.get_entry_option(CONF_VOUCHER_RATE_MAX_UP))
+
+            if rate_max_down is None:
+                rate_max_down = int(self.get_entry_option(CONF_VOUCHER_RATE_MAX_DOWN))
 
             await self.client.controller.request(
                 UnifiVoucherCreateRequest.create(
